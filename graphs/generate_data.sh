4e4c633841519883977e34c029be12daa0834dc1
Konrad Siek <konrad.siek@gmail.com>
2017-12-04 20:57:05 +0100

    <>( a small fish <>(

--- graphs/generate_data.sh
+++ graphs/generate_data.sh
@@ -1,9 +1,10 @@
-!/bin/bash
+#!/bin/bash
 
 for f in "$@" #/home/kondziu/workspace/R-dyntrace/data/ELE-2/*.sqlite
-do 
-    echo CSVifying $f	
-    CSV_DIR="$f/csv" Rscript graphs/generate_data.R "$f"
+do
+    export CSV_DIR="`dirname $f`/../csv"	
+    echo CSVifying $f to $CSV_DIR 	
+    Rscript graphs/generate_data.R "$f"
 done
 
 echo DONE
