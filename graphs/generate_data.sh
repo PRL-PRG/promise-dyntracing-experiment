2e08b818e2de30efaa9e5bc5e78248bef17249bb
Konrad Siek <konrad.siek@gmail.com>
2018-01-10 14:53:50 +0100

    Log CSV generation correctness.

--- graphs/generate_data.sh
+++ graphs/generate_data.sh
@@ -3,9 +3,13 @@
 for f in "$@" #/home/kondziu/workspace/R-dyntrace/data/ELE-2/*.sqlite
 do
     export CSV_DIR="`dirname $f`/../csv"/`basename $f .sqlite`	
+    CSV_ROOT_DIR="`dirname $f`/../csv"
     echo Extracting aggregate CSVs from $f to $CSV_DIR
     mkdir -p $CSV_DIR
-    Rscript graphs/generate_data.R "$f"
+    Rscript graphs/generate_data.R "$f" \
+        && echo ` basename "$f"`";TRUE" >> "$CSV_ROOT_DIR/health.csv" \
+        || echo ` basename "$f"`";FALSE" >> "$CSV_ROOT_DIR/health.csv"
+
     #R_LIBS=~/R/installed/ ../R-dyntrace/bin/Rscript graphs/generate_data.R "$f"
 done
 
