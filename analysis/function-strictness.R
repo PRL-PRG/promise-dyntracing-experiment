fd45f5cf32a1e0eabb850803449a8ee6634c90f9
Aviral Goel <goel.aviral@gmail.com>
2018-06-07 16:39:36 -0400

    Update function strictness analysis

--- analysis/function-strictness.R
+++ analysis/function-strictness.R
@@ -24,14 +24,61 @@ summarize_analyses <- function(analyses) {
     argument_usage_distribution %>%
     mutate(relative_function_count = function_count / total_function_count)
 
+  call_count <-
+    analyses$`function-name` %>%
+    group_by(function_id) %>%
+    summarize(call_count = sum(as.numeric(count)))
+
+  parameter_usage_distribution <-
+    analyses$`function-formal-parameter-usage-count` %>%
+    filter(usage == "E") %>%
+    group_by(function_id, formal_parameter_position) %>%
+    summarize(evaluation_count = sum(count)) %>%
+    ungroup() %>%
+    left_join(call_count, by = "function_id")
+
+  parameter_classification <-
+    parameter_usage_distribution %>%
+    mutate(classification = ifelse(evaluation_count >= call_count,
+                                   "Always Evaluated",
+                            ifelse(evaluation_count == 0,
+                                   "Never Evaluated",
+                                   "Sometimes Evaluated"))) %>%
+    group_by(classification) %>%
+    summarize(parameter_count = sum(as.numeric(n())))
+
+  total_parameter_count <- sum(parameter_classification$parameter_count)
+
+  parameter_classification <-
+    parameter_classification %>%
+    mutate(relative_parameter_count =
+             parameter_count / total_parameter_count)
+
+  function_classification <-
+    parameter_usage_distribution %>%
+    group_by(function_id) %>%
+    mutate(classification = ifelse(all(evaluation_count >= call_count),
+                                   "Strict",
+                            ifelse(any(evaluation_count == 0),
+                                   "Partial",
+                                   "Non Strict"))) %>%
+    ungroup() %>%
+    group_by(classification) %>%
+    summarize(function_count = sum(as.numeric(n()))) %>%
+    mutate(relative_function_count = function_count / total_function_count)
+
   list(argument_usage_count = argument_usage_count,
        argument_usage_distribution = argument_usage_distribution,
-       summary = tibble(total_function_count = total_function_count))
+       function_classification = function_classification,
+       parameter_classification = parameter_classification,
+       summary = tibble(total_function_count = total_function_count,
+                        total_parameter_count = total_parameter_count))
 }
 
 visualize_analyses <- function(analyses) {
 
   total_function_count <- analyses$summary$total_function_count
+  total_parameter_count <- analyses$summary$total_parameter_count
 
   function_strictness_ordering <-
     analyses$argument_usage_distribution %>%
@@ -45,7 +92,35 @@ visualize_analyses <- function(analyses) {
          title =  "Function count per argument evaluation order") +
     scale_fill_gdocs()
 
-  list(function_strictness_ordering = function_strictness_ordering)
+
+  parameter_classification <-
+    analyses$parameter_classification %>%
+    rename("Formal parameter classification" = classification) %>%
+    ggplot(aes(`Formal parameter classification`, relative_parameter_count)) +
+    geom_col() +
+    scale_y_continuous(sec.axis = sec_axis(~ . * total_parameter_count,
+                                           labels = count_labels),
+                       labels = relative_labels) +
+    labs(y = "Formal parameter count (%)",
+         title =  "Formal parameter distribution by evaluation mode") +
+    scale_fill_gdocs()
+
+  function_classification <-
+    analyses$function_classification %>%
+    rename("Function classification" = classification) %>%
+    ggplot(aes(`Function classification`, relative_function_count)) +
+    geom_col() +
+    scale_y_continuous(sec.axis = sec_axis(~ . * total_function_count,
+                                           labels = count_labels),
+                       labels = relative_labels) +
+    labs(y = "Function count (%)",
+         title =  "Function distribution by evaluation mode") +
+    scale_fill_gdocs()
+
+
+  list(function_strictness_ordering = function_strictness_ordering,
+       parameter_classification = parameter_classification,
+       function_classification = function_classification)
 }
 
 latex_analyses <- function(analyses) {
